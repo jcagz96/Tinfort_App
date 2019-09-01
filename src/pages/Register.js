d2b87dc0e549fa92d9d74a8b5db827d47e2a2859
jcagz96 <jcagz@iscte-iul.pt>
2019-09-01 03:42:54 +0100

    added upload avatar feature & upgrade register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
+import React, { FormData, useState } from 'react';
+import {View, Text, Image, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+import uploadLogo from '../assets/upload.png'
 
 //import api from '../services/api';
 
 export default function Register({ navigation }){
+
+    const [name, setName] = useState('');
+    const [user, setUser] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+
+
+    const [avatar, setAvatar] = useState(null);
+
 
     function handleLoginRoute(){
         console.log("entrou a na função handle login route");
@@ -11,15 +24,68 @@ export default function Register({ navigation }){
         navigation.navigate('Login');
     }
 
+    async function handlePhotoUpload(){
+
+        ImagePicker.showImagePicker({noData: true, mediaType:'photo'}, (response) => {
+            console.log('Response = ', response);
+          
+            if (response.didCancel) {
+              console.log('User cancelled image picker');
+            } else if (response.error) {
+              console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+              console.log('User tapped custom button: ', response.customButton);
+            } else {
+          
+              // You can also display the image using data:
+              // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+              setAvatar(response.uri)
+            }
+          });
+
+
+
+        /*
+        let formdata = new FormData();
+        formdata.append('file', );
+        formdata.append('name', );
+        formdata.append('fortniteUsername', );
+        formdata.append('password', );
+        formdata.append('email', );
+        formdata.append('plataform', );*/
+    }
+
+    function handleRegister(){
+        console.log(`avatar: ${avatar}    ,   name: ${name}   ,  fortniteusername: ${user}   email: ${email}   \n
+        password1: ${password}  ,  password2: ${repeatPassword}`);
+    }
+
     return (
 
         <View style={styles.container}>
+            { avatar === null &&
+            <TouchableOpacity onPress={handlePhotoUpload}>
+                <Image style={styles.uploadIcon} source={uploadLogo}/>
+            </TouchableOpacity>
+            }
+                
+            {
+                avatar && (
+                    <TouchableOpacity onPress={handlePhotoUpload}>
+                        <Image source={{uri : avatar}} style={styles.avatarImage}/>
+                    </TouchableOpacity>
+                )
+            }
+
             <TextInput
                 autoCapitalize="none"
                 autoCorrect={false}
                 placeholder="Name"
                 placeholderTextColor="#999"
                 style={styles.inputName}
+                value={name}
+                onChangeText={setName}
             />
             <TextInput
                 autoCapitalize="none"
@@ -27,6 +93,8 @@ export default function Register({ navigation }){
                 placeholder="Fortnite username"
                 placeholderTextColor="#999"
                 style={styles.inputFortniteUsername}
+                value={user}
+                onChangeText={setUser}
             />
             <TextInput
                 autoCapitalize="none"
@@ -34,6 +102,8 @@ export default function Register({ navigation }){
                 placeholder="Fortnite email"
                 placeholderTextColor="#999"
                 style={styles.inputFortniteEmail}
+                value={email}
+                onChangeText={setEmail}
             />
             <TextInput
                 autoCapitalize="none"
@@ -42,6 +112,8 @@ export default function Register({ navigation }){
                 placeholder="Fortnite password"
                 placeholderTextColor="#999"
                 style={styles.inputFortnitePassword}
+                value={password}
+                onChangeText={setPassword}
             />
             <TextInput
                 autoCapitalize="none"
@@ -50,8 +122,10 @@ export default function Register({ navigation }){
                 placeholder="Repeat password"
                 placeholderTextColor="#999"
                 style={styles.inputFortnitePassword}
+                value={repeatPassword}
+                onChangeText={setRepeatPassword}
             />
-            <TouchableOpacity style={styles.buttonRegister}>
+            <TouchableOpacity onPress={handleRegister} style={styles.buttonRegister}>
                 <Text style={styles.buttonTextRegister}>Register</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={handleLoginRoute} style={styles.buttonLogin}>
@@ -79,7 +153,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#DDD',
         borderRadius: 4,
-        marginTop: 20,
+        marginTop: 10,
         paddingHorizontal: 15,
       },
   
@@ -143,6 +217,21 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 16,
     },
+
+    uploadIcon: {
+        width: 120,
+        height: 120,
+    },
+
+    avatarImage : {
+        width: 160,
+        height: 160,
+        borderRadius: 80,
+        borderWidth: 1,
+        borderColor: '#FFF',
+        marginVertical:10,
+    },
+
 
 
 
